62097a622ecc5d9a3a1816318b01d6a0fcab5e12
ChuHieu <chuhieu020297@gmail.com>
2019-10-07 07:15:24 +0700

    update  8 - 10

--- app/src/main/java/com/hieu/MainActivity.java
+++ app/src/main/java/com/hieu/MainActivity.java
@@ -1,12 +1,18 @@
 package com.hieu;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,20 +20,28 @@ import com.hieu.activity.LibraryActivity;
 import com.hieu.activity.SettingsActivity;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Toolbar toolbar;
     private ImageView ivBottomLibrary;
     private ImageView ivBottomRecoder;
     private ImageView ivBottomSettings;
-
+    private MediaRecorder mAudioRecorder;
+    private ImageView ivRecord , ivPauseResume;
+    private String outputFile;
+    private int recordingStatus;
+    private int pauseStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mappingBottomNavigation();
-
+        createFile();
+        recordingStatus = 0;
+        pauseStatus = 0;
+        onRecordAudio();
 
     }
 
@@ -37,6 +51,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ivBottomRecoder = (ImageView) findViewById(R.id.iv_bottom_recoder);
         ivBottomSettings = (ImageView) findViewById(R.id.iv_bottom_settings);
 
+        ivPauseResume =(ImageView) findViewById(R.id.imageViewPauseResume);
+        ivRecord =(ImageView) findViewById(R.id.imageViewRecord);
+        ivPauseResume.setEnabled(false);
+
         ivBottomSettings.setOnClickListener(this);
         ivBottomLibrary.setOnClickListener(this);
 
@@ -45,10 +63,129 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private void createFile() {
-        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Recorder");
+        File file = new File(Environment.getExternalStorageDirectory(),"DemoRecorderApp");
+        outputFile ="/"+ file.getAbsolutePath()+"/RecordFile"+System.currentTimeMillis()+".mp3";
         if (!file.exists()) {
             file.mkdirs();
         }
+
+    }
+    private int isEnable(){
+        ivPauseResume.setEnabled(true);
+        recordingStatus = 1;
+        return recordingStatus;
+    }
+
+    private int isDisable(){
+        ivPauseResume.setEnabled(false);
+        recordingStatus = 0;
+        return  recordingStatus;
+
+    }
+
+    private  void  setupMediaRecorder(){
+        mAudioRecorder = new MediaRecorder();
+        mAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mAudioRecorder.setOutputFile(outputFile);
+    }
+
+    private  void startRecording(){
+        createFile();
+        // outputFile =Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator+"Test" + ".mp3";
+        setupMediaRecorder();
+        try {
+            mAudioRecorder.prepare();
+            mAudioRecorder.start();
+        } catch (IllegalStateException ise) {
+            // make something ...
+        } catch (IOException ioe) {
+            // make something
+        }
+        Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void pauseRecording(){
+        if (mAudioRecorder!=null){
+            mAudioRecorder.pause();
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void resumeRecording(){
+        if (mAudioRecorder!=null){
+            mAudioRecorder.resume();
+        }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void stopRecording(){
+        if (mAudioRecorder!=null){
+            mAudioRecorder.stop();
+            mAudioRecorder.release();
+            mAudioRecorder = null;
+            Toast.makeText(getApplicationContext(), "Audio Recorder successfully", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void playRecodingResult(){
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(outputFile);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            Toast.makeText(getApplicationContext(), "Playing Audio", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            // make something
+        }
+    }
+
+    private  void onRecordAudio(){
+        ivRecord.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(recordingStatus == 0){
+                    startRecording();
+                    ivRecord.setImageResource(R.drawable.ic_home_pause);
+                    recordingStatus =1;
+                    pauseStatus = 0;
+                    ivPauseResume.setImageResource(R.drawable.ic_home_pause);
+                    ivPauseResume.setEnabled(true);
+                    //isEnable();
+                }else {
+                    if(recordingStatus == 1){
+                        stopRecording();
+                        ivRecord.setImageResource(R.drawable.ic_home_record);
+                        recordingStatus =0;
+                        pauseStatus = 0;
+                        ivPauseResume.setImageResource(R.drawable.ic_home_pause);
+                        ivPauseResume.setEnabled(false);
+                    }
+                }
+            }
+        });
+        ivPauseResume.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                if(pauseStatus == 0) {
+                    pauseRecording();
+                    pauseStatus = 1;
+                    ivPauseResume.setImageResource(R.drawable.ic_home_play);
+                    Toast.makeText(getApplicationContext(), "Pause Recording", Toast.LENGTH_LONG).show();
+                }else{
+                    if(pauseStatus == 1){
+                        resumeRecording();
+                        pauseStatus = 0;
+                        ivPauseResume.setImageResource(R.drawable.ic_home_pause);
+                        Toast.makeText(getApplicationContext(), "Resume Recording", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 
 
